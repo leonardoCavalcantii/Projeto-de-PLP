16e34018eb51113ef8debf315fd4930f33c381d4
Evlima <limaemanuel06@gmail.com>
2023-08-28 21:51:10 -0300

    Create Paciente.hs
    rascunho de paciente

---
+++ Paciente.hs
@@ -0,0 +1,15 @@
+module Paciente where
+
+data Paciente = Paciente {
+    pacienteID :: Int,
+    nome :: String,
+    dataNascimento :: String,
+    endereco :: String,
+    contato :: String
+} deriving Show
+
+setNomePaciente :: Paciente -> String -> Paciente
+setNomePaciente paciente novoNome = paciente { nome = novoNome }
+
+setContatoPaciente :: Paciente -> String -> Paciente
+setContatoPaciente paciente novoContato = paciente { contato = novoContato }
