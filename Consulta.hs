7588be8a516db966ded3e9ce6cef40a6319b7381
root <root@Leonardo-Cavalcanti>
2023-09-04 19:37:55 -0300

    -3

--- Consulta.hs
+++ Consulta.hs
@@ -2,10 +2,12 @@ import Data.Time
 import Paciente
 import Medico
 
+
 data Consulta = Consulta {
+   
     consultaId :: Int,
-    --role :: a,
     medico :: Medico,
     paciente :: Paciente,
     dataHora :: UTCTime
+   
     }
--- Paciente.hs
+++ Paciente.hs
@@ -1,15 +1,15 @@
-import Data.Time
 
-data Paciente = Paciente {
-    pacienteID :: Int,
+
+data Paciente = Paciente
+  { pacienteID :: Int,
     nome :: String,
-    dataNascimento :: UTCTime,
+    dataNascimento :: String,
     endereco :: String,
     contato :: String
-    } --deriving Show
+  }  deriving Show
 
---setNomePaciente :: Paciente -> String -> Paciente
---setNomePaciente paciente novoNome = paciente { nome = novoNome }
+setNomePaciente :: Paciente -> String -> Paciente
+ setNomePaciente paciente novoNome = paciente { nome = novoNome }
 
---setContatoPaciente :: Paciente -> String -> Paciente
---setContatoPaciente paciente novoContato = paciente { contato = novoContato }
+ setContatoPaciente :: Paciente -> String -> Paciente
+    setContatoPaciente paciente novoContato = paciente { contato = novoContato }
