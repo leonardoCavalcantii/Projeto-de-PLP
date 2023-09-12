03f63297ec1035c60f7fde4d5a34f845202fc714
saraariadne <saara.ariadne@gmail.com>
2023-09-12 17:43:56 -0300

    Iniciando a classe Paciente

---
+++ Models/Paciente.hs
@@ -0,0 +1,12 @@
+data Paciente = Paciente { 
+    pacienteId :: Int,
+    nome :: String,
+    cpf :: String,
+    endereco :: String,
+    contato :: String,
+    altura :: Double,
+    peso :: Double,
+    grupoSanguineo :: String
+  }
+
+  -- Implementar os métodos dessa classe!
