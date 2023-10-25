069fbb33da5109646d501e9cd842efc0ef29216f
saraariadne <saara.ariadne@gmail.com>
2023-10-25 19:40:59 -0300

    povoando o bd de medicos ok :B

--- Prolog/bd_Medico.pl
+++ Prolog/bd_Medico.pl
@@ -1,4 +1,12 @@
 
 :- dynamic medico/6.
 
+medico("Rose", "endocrinologista", "123123-123", "83 11111-1111", "rose@email", "1212").
+medico("David", "cardiologista", "456456-456", "83 22222-2222", "david@email", "2323").
+medico("Sophia", "dermatologista", "789789-789", "83 33333-3333", "sophia@email", "3434").
+medico("Laura", "ginecologista", "101010-101", "83 44444-4444", "laura@email", "4545").
+medico("Gabriel", "ortopedista", "121212-121", "83 55555-5555", "gabriel@email", "5656").
+medico("Aline", "pediatra", "131313-131", "83 66666-6666", "aline@email", "6767").
+medico("Rafael", "oftalmologista", "141414-141", "83 77777-7777", "rafael@email", "7878").
+medico("Julia", "psiquiatra", "151515-151", "83 88888-8888", "julia@email", "8989").
 
