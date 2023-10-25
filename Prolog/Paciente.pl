f611656658ef368809562744acd597d05a5208a8
Leonardo Cavalcanti <81041338+leonardoCavalcantii@users.noreply.github.com>
2023-10-24 21:59:57 -0300

    ajustes na classe pacietne

--- Prolog/Paciente.pl
+++ Prolog/Paciente.pl
@@ -3,26 +3,35 @@ bd_Paciente:- consult('./bd_Paciente.pl').
 cadastraPaciente :-
 	bd_Paciente, nl,
 	nl, writeln("Digite seus dados: "),
+	
 	nl, writeln("nome: "),
 	read_line_to_string(user_input, Nome),
+	
 	nl, writeln("cpf: "),
 	read_line_to_string(user_input, CPF),
+	
 	nl, writeln("telefone: "),
 	read_line_to_string(user_input, Telefone),
+	
 	nl, writeln("peso: "),
 	read_line_to_string(user_input, Peso),
+	
 	nl, writeln("idade: "),
-	read_line_to_string(user_input, idade),
+	read_line_to_string(user_input, Idade),
+	
 	nl, writeln("grupo sanguineo: "),
 	read_line_to_string(user_input, Grupo_Sanguineo),
+	
 	nl, writeln("email: "),
 	read_line_to_string(user_input, Email),
+	
 	nl, writeln("senha: "),
-	read_line_to_string(user_input, Senha).
+	read_line_to_string(user_input, Senha),
+	
 	nl,
-	(get_emails_clientes(Emails), member(Email, Emails) -> nl, writeln("Email já cadastrado."), nl;
-	assertz(cliente(Nome, Email, Senha, Telefone)),
-	adicionaCliente,
+	(get_emails_paciente(Emails), member(Email, Emails) -> nl, writeln("Email já cadastrado."), nl;
+	assertz(cliente(Nome, CPF, Email, Telefone, Peso, Idade, Grupo_Sanguineo, Email, Senha)),
+	adicionaPacietne,
 	writeln("Cliente cadastrado com sucesso!"),nl),
 	fimMetodo.
 
