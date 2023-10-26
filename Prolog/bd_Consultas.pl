fb6f9a8aba1e0a148a4d6e4d2a3405b028e82c4d
GustavoAlvrega <gustavo.costa@ccc.ufcg.edu.br>
2023-10-26 08:11:26 -0300

    Atualizando

--- Prolog/bd_Consultas.pl
+++ Prolog/bd_Consultas.pl
@@ -11,6 +11,7 @@ consulta("7", "Antonio", "joana", "joana@email", "27/11/2023", "10:00", "Pendent
 consulta("8", "Paula", "ana", "ana@email", "28/11/2023", "14:30", "concluida").
 consulta("9", "Gustavo", "sara", "sara@email", "29/11/2023", "09:30", "Pendente").
 consulta("10", "Paula", "sara", "sara@email", "30/11/2023", "11:45", "Pendente").
-consulta("11", "Pedrita", "sara", "sara@email", "01/12/2023", "14:00", "Pendente").
 consulta("12", "Gustavo", "sara", "sara@email", "02/12/2023", "10:30", "Concluida").
 consulta("13", "Pedro", "sara", "sara@email", "03/12/2023", "13:15", "Concluida").
+consulta("11", "Gustavo", "Sara", "sara@email", "26/10/2023", "00:10", "Pendente").
+
